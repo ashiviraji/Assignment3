0199477055d3e43b897f506e1153f20dd55520f9
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2020-05-29 11:07:59 +0530

    Assignment3

---
+++ Assignment3/src/quiz3.scala
@@ -0,0 +1,18 @@
+
+
+object quiz3 {
+  def main(args:Array[String]){
+    
+   def sum(n:Int):Int=n match{
+      case x if(x==0)=>x
+      case x if(x>0)=> x+sum(x-1)
+   }
+  println(sum(5))
+  
+    
+    
+      
+      
+    
+  }
+  }
